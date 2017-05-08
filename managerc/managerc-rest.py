94c51452ba5abb7b85b4523aeaa2d23d50ada005
Louis <louis@vimeo.com>
2017-05-08 00:03:15 -0400

    ID added, POST working

--- managerc/managerc-rest.py
+++ managerc/managerc-rest.py
@@ -2,12 +2,23 @@ import falcon
 import elasticsearch
 import curator
 import json
+import uuid
 
 
+# Managerc specific indicies.
 TASKS_INDEX = "managerc-tasks"
 LOGS_INDEX = "managerc-logs"
 
+# ElasticSearch queries
 QUERY_ALL = {"query": {"match_all": {}}}
+
+# Supported values for json input to service. This is used mostly for json validation.
+SUPPORTED_TASKS = ('close', 'forcemerge', 'delete')
+SUPPORTED_TASK_INTERVAL = ('daily', 'monthly')
+SUPPORTED_FILTER_TYPES = ('age',)
+SUPPORTED_FILTER_DIRECTIONS = ('older', 'younger')
+SUPPORTED_FILTER_UNITS = ('days',)
+
 
 '''
 Hooks
@@ -85,39 +96,81 @@ class IndexListResource(object):
         resp.body = results
 
 
-class TaskParamsExecption(Exception):
+class TaskException(Exception):
     def __init__(self, message):
         self.message = message
 
-class TaskPOSTData(object):
+
+class TaskDoc(object):
     '''
-    This class is used to validate json schema (keys). Value validation will occur at the curator API.
+    Class for representing a valid Task document in Elasticsearch. If instantiation does not fail a valid 
+    Task document was created. This class can be used with the __dict__ meta field, passing a dictionary 
+    representation of the class to the ElasticSearch client. 
     '''
     def __init__(self, POST_data):
+        '''
+        Init validates POST data provided by the client along with generating a unique ID for this document. 
+        :param POST_data: Deserialized python object created from client POST data. 
+        
+        Expected POST schema (refer to global variables at top of source for allowed values): 
+        {
+            "id": "UUID", # generated within this function
+            "type": "close",
+            "interval": "daily",
+            "time": "15:00:00",
+            "filter": {
+                "type": "age",
+                "direction": "older | younger",
+                "unit": "days | months",
+                "unit_count": 5
+            }
+        }
+        
+        '''
+        self.id = None
         self.type = None
         self.interval = None
         self.time = None
         self.filter = None
 
         # Check required top level keys are present
-        top_level_keys = ["type", "interval", "time", "filter"]
+        top_level_keys = ["type", "interval", "time", "filter", "id"]
         keys = list(POST_data.keys())
 
         if not all((k in top_level_keys) for k in keys):
-            raise TaskParamsExecption(message="POST data is missing a required key(s).")
+            raise TaskException(message="POST data is missing a required key(s).")
 
         # Check required filter keys are present
-        filter_keys = ["direction", "unit", "unit_count"]
-        keys = list(POST_data.keys())
+        filter_keys = ["direction", "unit", "unit_count", "type"]
+        keys = list(POST_data["filter"].keys())
 
         if not all((k in filter_keys) for k in keys):
-            raise TaskParamsExecption(message="POST data is missing required keys(s) in the filter object")
+            raise TaskException(message="POST data is missing required keys(s) in the filter object")
+
+        # Check top level values are valid
+        # TODO: filter time string once format decided
+        if (POST_data["type"] not in SUPPORTED_TASKS) \
+                or (POST_data["interval"] not in SUPPORTED_TASK_INTERVAL):
+            raise TaskException(message="Provided value unknown")
+
+        # Check filter values are valid.
+        filter = POST_data["filter"]
+        if (filter["type"] not in SUPPORTED_FILTER_TYPES) \
+                or (filter["direction"] not in SUPPORTED_FILTER_DIRECTIONS) \
+                or (filter["unit"] not in SUPPORTED_FILTER_UNITS) \
+                or (not isinstance(filter["unit_count"], int)):
+            raise TaskException(message="Provided value for filter unknown")
+
+        # Generate ID
+        id = uuid.uuid1()
 
         # Schema is validated, populate class object.
+        self.id = id
         self.type = POST_data["type"]
         self.interval = POST_data["interval"]
         self.time = POST_data["time"]
         self.filter = POST_data["filter"]
+
 
 class TaskResource(object):
     '''
@@ -144,6 +197,28 @@ class TaskResource(object):
 
         resp.body = results
 
+    def on_post(self, req, resp):
+
+        # Make sure content_length is greater then 0
+        if req.content_length < 1:
+            raise falcon.HTTPBadRequest(body="No data sent with POST")
+
+        # Create POST_data dictionary
+        data = req.stream.read(req.content_length)
+        POST_data = json.loads(data, encoding='utf-8')
+
+        # Instantiate TaskDoc
+        try:
+            doc = TaskDoc(POST_data)
+        except TaskException as e:
+            raise falcon.HTTPError(falcon.HTTP_400, 'Bad Request', e.message)
+
+        # Send validated document to managerc-tasks
+        try:
+            self.es_client.index(index=TASKS_INDEX, doc_type="task", body=doc.__dict__, id=id)
+        except:
+            raise falcon.HTTPInternalServerError(description=e.message)
+
 
 def main():
     # WSGI callable
@@ -161,6 +236,7 @@ def main():
     app.add_route('/ping', ping)
     app.add_route('/indexList', index_list)
     app.add_route('/tasks', tasks)
+
     return app
 
 app = main()

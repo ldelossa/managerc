19cdb61b59d32a6ecc11f5e3fb4794b2a3f00754
Louis DeLosSantos <ldelossa.ld@gmail.com>
2017-06-17 01:17:49 -0400

    WIP - Daemon working

--- managerc/managerc_daemon.py
+++ managerc/managerc_daemon.py
@@ -2,6 +2,7 @@ import elasticsearch
 import curator
 from curator.actions import ForceMerge
 from datetime import datetime, timedelta
+from multiprocessing import Process
 import time
 
 # Managerc specific indicies.
@@ -17,12 +18,14 @@ class ManagerCException(Exception):
 
 class ManagerC(object):
     def __init__(self, es_client):
+        self.debug_counter = 0
         self.es_client = es_client
+        self.processes = {}
         self._ilo = None
         self.actions_map = {
             "forcemerge": ForceMerge
         }
-        # This will be populated via the ILO setter.
+        # This will be populated via the ilo setter.
         self.filters_map = {}
 
     @property
@@ -30,18 +33,19 @@ class ManagerC(object):
         return self._ilo
 
     @ilo.setter
-    def ilo(self, i):
+    def ilo(self, ilo):
         # Set self.ilo to passed in ilo instance
-        self._ilo = i
+        self._ilo = ilo
 
         # update filter maps to reflect current instance of IndexList
         self.filters_map["filter_by_age"] = self._ilo.filter_by_age
 
-
     def _get_tasks(self):
+        self.debug_counter += 1
+        print(self.debug_counter)
 
         search_results = self.es_client.search(index=TASKS_INDEX, doc_type="task", body=QUERY_ALL)
-
+        print("Search Results: {}".format(search_results))
         if search_results['hits']['total'] < 1:
             raise ManagerCException(message="ManagerC could find any tasks")
 
@@ -50,6 +54,11 @@ class ManagerC(object):
         return docs
 
     def _should_run_task(self, task):
+
+        # Is task currently executing?
+        if task["id"] in self.processes:
+            print("Task is currently being executed")
+            return
 
         # Get current time
         current_time = datetime.utcnow()
@@ -61,7 +70,8 @@ class ManagerC(object):
         print("Target Time: {}".format(target_time))
 
         # If task has been ran...
-        if task["last_ran"]:
+
+        if "last_ran" in task and task["last_ran"] is not None:
             last_ran = datetime.strptime(task["last_ran"], "%d:%H:%M:%S")
             print("Last Ran: {}".format(target_time))
 
@@ -84,9 +94,10 @@ class ManagerC(object):
         return False
 
     def _run_task(self, task):
+        es = elasticsearch.Elasticsearch(hosts=["localhost"])
         # Create IndexList for duration of this function call.
         # Requires this function to always be accessed synchronously.
-        self.ilo = curator.IndexList(self.es_client)
+        self.ilo = curator.IndexList(es)
 
         # Find task type and lookup in actions_map
         try:
@@ -96,39 +107,78 @@ class ManagerC(object):
             return
 
         # Find filters in filter map
-        for filter in task["filters"]:
+        for filt in task["filters"]:
 
-            filter_func = self.filters_map[filter["type"]]
+            # Get the appropriate filter function via the filters_map
+            filter_func = self.filters_map[filt["type"]]
+
             # filter out type from filter
-            params = {k: filter[k] for k in filter if k != "type"}
-            print(filter_func == self.ilo.filter_by_age)
-            #filter_func(**params)
+            params = {k: filt[k] for k in filt if k != "type"}
+            filter_func(**params)
 
-        #action = action(self.ilo)
-        #action.do_action()
+        action = action(self.ilo, **task["options"])
+
+        name = "{}-{}".format(task["type"], task["id"])
+        p = Process(target=self._run_task_threadable, args=(action,task), name=name)
+        self.processes[task["id"]] = p
+
+        p.start()
+
+        return
+
+    def _run_task_threadable(self, action, task):
+
+        action.do_action()
+
+        # Update last_ran
+        current_time = datetime.utcnow().strftime('%d:%H:%M:%S')
+        task["last_ran"] = current_time
+
+        # Update record
+        es = elasticsearch.Elasticsearch(hosts=["localhost"])
+        print("task: {}".format(task))
+        es.index(index=TASKS_INDEX, doc_type="task", id=task["id"], body=task)
 
     def start(self):
 
         while True:
 
+            # Remove stopped processes
+            removable=[]
+            for process in self.processes:
+                if not self.processes[process].is_alive():
+                    removable.append(process)
+
+            print("Removable {}".format(removable))
+            for removable_process in removable:
+                print("Removing process {} from list".format(removable_process))
+                del self.processes[removable_process]
+                removable.remove(removable_process)
+
             # Get tasks from ES:
-            #tasks = self._get_tasks()
+            tasks = self._get_tasks()
+            print(tasks)
 
             # Get list of runnable tasks
             runnables = [task for task in tasks if self._should_run_task(task)]
+            print("Runnables {}".format(runnables))
 
-            # Run curator tasks (implement multiprocessing here)
+            # Run tasks
+            for task in runnables:
+                self._run_task(task)
+
+            print(self.processes)
 
             # Sleep
-            time.sleep(.5)
+            time.sleep(1)
 
-# if __name__ == "__main__":
-#     # ES Client
-#     es = elasticsearch.Elasticsearch(hosts=["localhost"])
-#
-#     # New Manager
-#     m = ManagerC(es)
-#     m.start()
+if __name__ == "__main__":
+    # ES Client
+    es = elasticsearch.Elasticsearch(hosts=["localhost"])
+
+    # New Manager
+    m = ManagerC(es)
+    m.start()
 
 
 
